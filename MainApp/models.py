2f99b9991d5616103ee509cd6322314902f7a542
Evgeniy <chernyshev.evgeniy@gmail.com>
2022-09-23 17:39:59 +0300

    2309-1

---
+++ MainApp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Item(models.Model):
+   name  = models.CharField(max_length=100)
+   brand = models.CharField(max_length=100)
+   count = models.PositiveIntegerField()
+
+   def __repr__(self):
+      return f"Item: {self.name}/{self} count: {self.count}"
---
+++ MainApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseNotFound, Http404
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
+
+def mainpage(request):
+    return render(request, 'index.html')
+
+def item(request, item_id: int):
+    try:
+        item = Item.objects.get(pk=item_id)
+    except ObjectDoesNotExist:
+        raise Http404(f"Item with id={item_id} not found")
+    context = {
+        "item": item
+    }
+    return render(request, 'item.html', context)
+    # return HttpResponseNotFound(f"Item with id={item_id} not found")
+    raise Http404(f"Item with id={item_id} not found")
+
+def items_list(request):
+    items = Item.objects.all()
+    context = {
+        "items": items
+    }
+    return render(request, 'items_list.html', context)
